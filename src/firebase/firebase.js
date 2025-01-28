cadab947475c8cdf87c2f103d8f606349de64cf2
sujit12x10 <sujit12x10@gmail.com>
2025-01-28 10:00:12 +0530

    testing firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,16 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 // Firebase Configuration
-const firebseConfig = {
-    apiKey: String(import.meta.env.VITE_FIREBASE_APIKEY),
-    authDomain: String(import.meta.env.VITE_FIREBASE_AUTHDOMAIN),
-    projectId: String(import.meta.env.VITE_FIREBASE_PROJECTID),
-    storageBucket: String(import.meta.env.VITE_FIREBASE_STORAGEBUCKET),
-    messagingSenderID: String(import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID),
-    appId: String(import.meta.env.VITE_FIREBASE_APPID),
-    // databaseURL: String(import.meta.env.VITE_FIREBASE_DATABASEURL)
-}
+const firebaseConfig = {
+    apiKey: "AIzaSyCheUl8CB4JefJEkQ2XiOv4Z2vy1fqu0AA",
+    authDomain: "ecom-a1e8c.firebaseapp.com",
+    databaseURL: "https://ecom-a1e8c-default-rtdb.firebaseio.com",
+    projectId: "ecom-a1e8c",
+    storageBucket: "ecom-a1e8c.firebasestorage.app",
+    messagingSenderId: "610767168068",
+    appId: "1:610767168068:web:ff3efebe096310b3cd1211"
+};
 
 // exporting app & auth
-export const app = initializeApp(firebseConfig)
+export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
